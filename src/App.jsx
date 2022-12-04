c0932107b9419e54096a8d8efa9ff807a50e7ae7
2DC <kkts9308@gmail.com>
2022-12-04 23:42:03 +0900

    useRef 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
-import {useContext} from "react"
-import {UserInfo} from "./context"
+import React, { useState, useRef } from 'react'
+import './app.css'
 
-const App = () => {
+function App() {
+  const [emailValue, setEmailValue] = useState('')
+  const [pwValue, setPwValue] = useState('')
+  const emailInput = useRef(null)
+  const pwInput = useRef(null)
+
+  const inputCheck = e => {
+    e.preventDefault()
+    if (emailInput.current.value === "") {
+      alert("이메일을 입력해주세요.")
+      emailInput.current.focus()
+      return
+    }
+    if (pwInput.current.value === "") {
+      alert("비번을 입력해주세요.")
+      pwInput.current.focus()
+      return
+    }
+
+    setEmailValue(emailInput.current.value)
+    setPwValue(pwInput.current.value)
+  }
+
   return (
-    <Hello2DC />
+      <form>
+        <label>
+          이메일 : <input type="email" ref={emailInput} />
+        </label>
+        <label>
+          비밀번호 : <input type="password"  ref={pwInput}/>
+        </label>
+
+        <button type='submit' style={{width: "100px"}} onClick={inputCheck}>
+          로그인
+        </button>
+        <div>입력한 이메일 : {emailValue}</div>
+        <div>입력한 비밀번호 : {pwValue}</div>
+      </form>
   )
 }
 
-const Hello2DC = () => {
-  const {name, id} = useContext(UserInfo)
-  return (
-    <div>
-      <h2>{name}</h2>
-      <strong>{id}</strong>
-    </div>
-  )
-}
-
-export default App
+export default App;
--- src/context.jsx
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react"
-
-export const UserInfo = createContext({name: "2DC", id: "2DC짱짱맨"})
