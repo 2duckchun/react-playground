0077005271d95b695db973a11b61f3c7d4e55d0a
2DC <kkts9308@gmail.com>
2022-12-11 22:50:03 +0900

    리액트 라우터 실습2(useNavigate)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 import './app.css'
 
 
 function App() {
+  const navigate = useNavigate()
 
   return (
     <div>
+      <h1 onClick={() => navigate('/detail')}>디테일 페이지</h1>
+      <h1 onClick={() => navigate('/about')}>어바웃 페이지</h1>
+      <h1 onClick={() => navigate('/other')}>기타 페이지</h1>
+      <h1 onClick={() => navigate(-1)}>뒤로</h1>
+      <h1 onClick={() => navigate(1)}>앞으로</h1>
       <Routes>
         <Route path='/' element={<h1>킹스랜딩</h1>}></Route>
         <Route path='/detail' element={<Detail/>}></Route>
         <Route path='/about' element={<About/>}></Route>
         <Route path='/other' element={<div>기타 페이지 입니다.</div>}></Route>
+        <Route path='*' element={<div>404 페이지입니다.</div>}></Route>
       </Routes>
-      <h1><Link to="/detail">디테일 페이지</Link></h1>
-      <h1><Link to="/about">어바웃 페이지</Link></h1>
-      <h1><Link to="/other">기타 페이지</Link></h1>
+
     </div>
   )
 }
